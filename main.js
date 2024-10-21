58182aa0e13f78acc6bc971539301a8ba87a1033
Christian Schmidt <cjschmidt33@gmail.com>
2024-10-20 22:21:56 -0400

    Add comments for clarity

--- main.js
+++ main.js
@@ -7,16 +7,16 @@
 // We will now add event listeners for the size dropdown, which will update the price based on the size selected.
 // This task also merged the 2nd and 3rd tasks, modifying the stock availability in the same task.
 
-const priceElement = document.getElementById("product-price");
-const sizeSelector = document.getElementById("size-selector");
+const priceElement = document.getElementById("product-price"); //product-price in HTML is now known as priceElement here
+const sizeSelector = document.getElementById("size-selector"); //size-selector in HTML is now known as sizeSelector here
 
 
 
-const purchaseButton = document.getElementById("purchase-button");
+const purchaseButton = document.getElementById("purchase-button"); //purchase-button in HTML is now known as purchaseButton here
 
 function updatePurchaseButton() {
-    const selectedOption = sizeSelector.options[sizeSelector.selectedIndex];
-    const stockStatus = selectedOption.getAttribute('data-stock');
+    const selectedOption = sizeSelector.options[sizeSelector.selectedIndex]; 
+    const stockStatus = selectedOption.getAttribute('data-stock'); //data stock in HTML has in stock or out of stock, and the purchase button will be accessible or disabled due to whichever one it is.
 
     if (stockStatus === 'out-of-stock') {
         purchaseButton.disabled = true;
@@ -25,12 +25,12 @@ function updatePurchaseButton() {
     }
 }
 
-sizeSelector.addEventListener("change", (event) => {
+sizeSelector.addEventListener("change", (event) => { //when the option in sizeSelector is changed, it will update the selected price at the top of the screen.
     const selectedPrice = event.target.value;
     priceElement.textContent = `$${selectedPrice}`
 })
 
-sizeSelector.addEventListener("change", updatePurchaseButton);
+sizeSelector.addEventListener("change", updatePurchaseButton); 
 
 updatePurchaseButton();
 
